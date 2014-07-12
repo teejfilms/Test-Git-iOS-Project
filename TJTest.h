0888cdbe4af2a07fe25b77ea78e44d260305ae54
TJ Gleason <TJ@TJs-MacBook-Pro.local>
2014-07-12 15:34:12 -0700

    added a test class

---
+++ TJTest.h
@@ -0,0 +1,13 @@
+//
+//  TJTest.h
+//  Git Test
+//
+//  Created by TJ Gleason on 7/12/14.
+//  Copyright (c) 2014 TJG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TJTest : NSObject
+
+@end
---
+++ TJTest.m
@@ -0,0 +1,13 @@
+//
+//  TJTest.m
+//  Git Test
+//
+//  Created by TJ Gleason on 7/12/14.
+//  Copyright (c) 2014 TJG. All rights reserved.
+//
+
+#import "TJTest.h"
+
+@implementation TJTest
+
+@end
